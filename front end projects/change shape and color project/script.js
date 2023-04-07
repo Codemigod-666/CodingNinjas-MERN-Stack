ea0741e35d2115569eeb9896ba452f09bfe2dbfb
Codemigod-666 <vikay434@gmail.com>
2023-04-07 13:46:10 +0530

    node connect commit

--- front end projects/change shape and color project/script.js
+++ front end projects/change shape and color project/script.js
@@ -1,5 +1,10 @@
 "use strict"
 
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
+const document = dom.window.document;
+
 var available_colors = [
     "red",
     "yellow",
@@ -20,13 +25,19 @@ var available_colors = [
     "triangle",
     "circle"
   ];
+  
+  if(document.getElementsByClassName("btn1")[0]){
+    document.getElementsByClassName('btn1')[0].addEventListener("click",function(){
+      var nextColor = available_colors[Math.floor(Math.random()*11)];
+      document.getElementsByClassName("inner")[0].style.backgroundColor = nextColor;
+    });
+  }
 
-  document.getElementsByClassName("btn1")[0].addEventListener("click",function(){
-    var nextColor = available_colors[Math.floor(Math.random()*11)];
-    document.getElementsByClassName("inner")[0].style.backgroundColor = nextColor;
-  });
+  
 
-  document.getElementsByClassName("btn2")[0].addEventListener("click", function(){
-    var nextShape = available_shapes[Math.floor(Math.random()*4)];
-    document.getElementById("shaper").setAttribute("class",nextShape);
-  })
+  if(document.getElementsByClassName("btn2")[0]){
+    document.getElementsByClassName('btn2')[0].addEventListener("click", function(){
+      var nextShape = available_shapes[Math.floor(Math.random()*4)];
+      document.getElementById("shaper").setAttribute("class",nextShape);
+    });
+  }
